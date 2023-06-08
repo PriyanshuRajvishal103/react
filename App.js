d09aa1930dd9864825db3171b49f93ba192bad7c
Priyanshu Raj <121234776+PriyanshuRajvishal103@users.noreply.github.com>
2023-06-08 09:36:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="name">
+    <h1>
+      Welcome to my page
+    </h1>
+    <h2>
+      how are you doing
+    </h2>
+   </div>
+  );
+}
+
+export default App;
